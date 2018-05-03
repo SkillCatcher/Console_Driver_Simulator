70422a18b37ec12ea762c66c80ac2cb6f93b2cef
SkillCatcher <marek.rudkiewicz@gmail.com>
2018-05-03 23:40:28 +0200

    Changed class Machine

--- src/main/java/driver/simulator/machines/Machine.java
+++ src/main/java/driver/simulator/machines/Machine.java
@@ -2,49 +2,64 @@ package driver.simulator.machines;
 
 public class Machine {
 
-    private boolean isAccesible;
-    private boolean isBroken;
+    private String name;
+    private double currentSpeed;
+    private double maxSpeed;
+    private double acceleration;
+    private double maxAcceleration;
+    private double breaksForce;
 
-    public boolean isAccesible() {
-        return isAccesible;
+    public String getName() {
+        return name;
     }
 
-    public boolean isBroken() {
-        return isBroken;
+    public double getCurrentSpeed() {
+        return currentSpeed;
     }
 
-    public void setAccesible(boolean accesible) {
-        isAccesible = accesible;
+    public double getMaxSpeed() {
+        return maxSpeed;
     }
 
-    public void setBroken(boolean broken) {
-        isBroken = broken;
+    public double getAcceleration() {
+        return acceleration;
     }
 
-    public Machine() {
-        this.isAccesible = true;
-        this.isBroken = false;
+    public double getMaxAcceleration() {
+        return maxAcceleration;
     }
 
-    public void getIn(String name) {
-        if(isAccesible && !isBroken) {
-            this.isAccesible = false;
-            System.out.println("You got in the " + name);
+    public double getBreaksForce() {
+        return breaksForce;
+    }
+
+    public Machine(String name, double maxSpeed, double maxAcceleration, double breaksForce) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+        this.maxAcceleration = maxAcceleration;
+        this.acceleration = 0.0;
+        this.currentSpeed = 0.0;
+        this.breaksForce = breaksForce;
+    }
+
+    private void speedUp(double accelerate) {
+        if(acceleration + accelerate > maxAcceleration) {
+            acceleration = maxAcceleration;
+        } else if(acceleration + accelerate < breaksForce*(-1)) {
+            acceleration = breaksForce*(-1);
         } else {
-            System.out.println(name + " is not accesible or broken. Please choose another machine.");
+            acceleration += accelerate;
         }
     }
 
-    public void getOut(String name) {
-        System.out.println("You got out of " + name);
-    }
-
-    public void fix(String name) {
-        if(isBroken) {
-            this.isBroken = false;
-            System.out.println("You have successfully repaired the " + name + ". Now you can use it.");
+    public void speedChange(double accelerate) {
+        speedUp(accelerate);
+        if(currentSpeed + acceleration > maxSpeed) {
+            currentSpeed = maxSpeed;
+        } else if(currentSpeed + acceleration < 0) {
+            currentSpeed = 0;
         } else {
-            System.out.println(name + " is not broken. Chill out...");
+            currentSpeed += acceleration;
         }
     }
 }
